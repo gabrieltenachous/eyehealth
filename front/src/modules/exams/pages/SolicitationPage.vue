1cc7b72f0021e14b92e3eae715ed213e4c47b9ba
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-08-01 09:15:50 -0300

    feat(exams): fluxo completo com modais de pacote funcionando

--- front/src/modules/exams/pages/SolicitationPage.vue
+++ front/src/modules/exams/pages/SolicitationPage.vue
@@ -4,10 +4,12 @@
       <h1 class="text-2xl font-bold">Solicitação de Exames</h1>
 
       <div class="flex gap-2">
-        <BaseButton @click="showListModal = true" variant="secondary"
-          >Pacotes</BaseButton
-        >
-        <BaseButton @click="showCreateModal = true">Criar Pacote</BaseButton>
+        <BaseButton @click="showListModal = true" variant="secondary">
+          Pacotes
+        </BaseButton>
+        <BaseButton @click="showCreateModal = true">
+          Criar Pacote
+        </BaseButton>
       </div>
     </div>
 
@@ -27,7 +29,7 @@
     <PackageCreateModal
       :visible="showCreateModal"
       :exams="exams"
-      @close="showCreateModal = false"
+      @close="handleCloseCreateModal"
     />
   </div>
 </template>
@@ -35,7 +37,7 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted, computed } from 'vue'
 import { useExamStore, usePackageStore } from '@/modules/exams'
-import { fetchExams } from '@/modules/exams'
+import { fetchExams, fetchPackages } from '@/modules/exams'
 import ExamSelection from '@/modules/exams/components/ExamSelection.vue'
 import PackageListModal from '@/modules/exams/components/PackageListModal.vue'
 import PackageCreateModal from '@/modules/exams/components/PackageCreateModal.vue'
@@ -57,9 +59,10 @@ export default defineComponent({
     const packages = computed(() => packageStore.packages)
 
     const selected = ref<Exam[]>([])
-
     const showListModal = ref(false)
     const showCreateModal = ref(false)
+
+    const exams = computed(() => store.exams)
 
     const handleSelect = (exams: Exam[]) => {
       selected.value = exams
@@ -70,23 +73,31 @@ export default defineComponent({
       showListModal.value = false
     }
 
-    const exams = computed(() => store.exams)
+    const handleCloseCreateModal = async () => {
+      showCreateModal.value = false
+      const latestPackages = await fetchPackages()
+      packageStore.setPackages(latestPackages)
+    }
 
     onMounted(async () => {
       if (!store.exams.length) {
         const data = await fetchExams()
         store.setExams(data)
       }
+
+      const pkgData = await fetchPackages()
+      packageStore.setPackages(pkgData)
     })
 
     return {
       exams,
       selected,
       showListModal,
-      packages,
       showCreateModal,
+      packages,
       handleSelect,
       handlePackageSelect,
+      handleCloseCreateModal,
     }
   },
 })
