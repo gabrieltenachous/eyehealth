1eecec6a28f7fb88fe5a01440d547521082bbde8
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-08-01 10:50:09 -0300

    feat(router): header com navegação real

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="min-h-screen bg-gray-100 text-gray-800 font-sans">
-    <header class="bg-white shadow p-4 mb-4">
-      <h1 class="text-xl font-bold text-blue-600">EyeCare Health - Exames</h1>
-    </header>
-
-    <main class="container mx-auto px-4 md:px-8">
-      <router-view />
-    </main>
+  <div>
+    <AppHeader />
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
+import AppHeader from '@/components/AppHeader.vue';
+
 export default {
   name: 'App',
-}
+  components: {
+    AppHeader,
+  },
+};
 </script>
