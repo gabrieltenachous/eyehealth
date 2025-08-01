6b683e16d4739a72f0be4b4a0a213e3775d24f12
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-08-01 11:20:33 -0300

    feat(services): examService.createExam & bulkCreate

--- front/src/modules/exams/services/examService.ts
+++ front/src/modules/exams/services/examService.ts
@@ -1,7 +1,18 @@
 import api from '@/services/api'
 import { Exam } from '@/modules/exams/interfaces/Exam'
+import type { ApiResponse } from '@/services/api'
 
 export async function fetchExams(): Promise<Exam[]> {
-  const response = await api.get('/exams')
+  const response = await api.get<ApiResponse<Exam[]>>('/exams')
   return response.data.data
+}
+
+export async function createExam(dto: Partial<Exam>): Promise<Exam> {
+  const { data } = await api.post<ApiResponse<Exam>>('/exams', dto)
+  return data.data
+}
+
+export async function bulkCreateExams(dtos: Partial<Exam>[]): Promise<Exam[]> {
+  const { data } = await api.post<ApiResponse<Exam[]>>('/exams/bulk', { exams: dtos })
+  return data.data
 }
